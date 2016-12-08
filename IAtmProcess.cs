4d3bde5366f1bdb059aa198e9f6d896ba9985bd6
ummis <swathi.ummineni@campsystems.com>
2016-12-08 19:32:00 +0530

    first commit

---
+++ IAtmProcess.cs
@@ -0,0 +1,103 @@
+﻿namespace _18November2016
+{
+    interface IAtmProcess
+    {
+        Active active();
+
+        Closed Closed();
+
+        InOperative InOperative();
+    }
+
+    interface ITransactions
+    {
+        string WithDraw();
+
+        string Deposit();
+
+        string BalanceEnquiry();
+
+        string accountStatus();
+    }
+
+    public abstract class Account : ITransactions
+    {
+        public virtual string WithDraw()
+        {
+            return true.ToString();
+        }
+
+        public virtual string Deposit()
+        {
+            return true.ToString();
+        }
+
+        public virtual string BalanceEnquiry()
+        {
+            return true.ToString();
+        }
+
+        public virtual string accountStatus()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class InOperative : Account
+    {
+        private string _accountStatus = "In-Operative";
+
+        public override string WithDraw()
+        {
+            _accountStatus = "Active";
+            return base.WithDraw();
+        }
+
+        public override string Deposit()
+        {
+            _accountStatus = "Active";
+            return base.Deposit();
+        }
+        public override string accountStatus()
+        {
+            return _accountStatus;
+        }
+    }
+
+
+    public class Closed : Account
+    {
+        public override string WithDraw()
+        {
+            return false.ToString();
+        }
+
+        public override string Deposit()
+        {
+            return false.ToString();
+        }
+
+        public override string BalanceEnquiry()
+        {
+            return false.ToString();
+        }
+        public override string accountStatus()
+        {
+            return "Closed";
+        }
+    }
+
+
+   public class Active : Account
+    {
+       public override string accountStatus()
+       {
+           return "Active";
+       }
+    }
+
+
+   
+
+   
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _18November2016
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter operation type"); //withdraw, deposit, balance enquiry
+            var operationType = Console.ReadLine();
+            Console.WriteLine("Enter account type"); // active, close, in-operative
+            var accountType = Console.ReadLine();
+            var operationStatus = string.Empty;
+            var accountStatus = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(operationType) && string.IsNullOrWhiteSpace(accountType.ToUpper())) return;
+            
+            var accountTypeObj = GetAccountType(accountType);
+
+
+            if (accountTypeObj != null)
+            {
+                switch (operationType.ToUpper())
+                {
+                    case "WITHDRAW":
+                    {
+                        operationStatus = accountTypeObj.WithDraw();
+                        accountStatus = accountTypeObj.accountStatus();
+                        break;
+                    }
+
+                    case "DEPOSIT":
+                    {
+                        operationStatus = accountTypeObj.WithDraw();
+                        accountStatus = accountTypeObj.accountStatus();
+                        break;
+                    }
+
+                    case "BALANCEENQUIRY":
+                    {
+                        operationStatus = accountTypeObj.WithDraw();
+                        accountStatus = accountTypeObj.accountStatus();
+                        break;
+                    }
+
+                }
+            }
+            
+            Console.WriteLine(operationStatus);
+            Console.WriteLine(accountStatus);
+            Console.ReadLine();
+
+        }
+
+        private static ITransactions GetAccountType(string accountType)
+        {
+            switch (accountType.ToUpper())
+            {
+                case "ACTIVE":
+                {
+                    return new Active();
+                }
+                case "CLOSED":
+                {
+                    return new Closed();
+                }
+
+                case "INOPERATIVE":
+                {
+                    return new InOperative();
+                }
+               
+            }
+            return null;
+        }
+    }
+}
+
